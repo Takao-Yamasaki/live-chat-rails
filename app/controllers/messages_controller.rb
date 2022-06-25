b989777571df56d922349a29f393982a8fae53cb
Takao-Yamasaki <captain_kaeru@yahoo.co.jp>
2022-06-25 17:17:33 +0900

    一覧取得のメソッドを設定

---
+++ app/controllers/messages_controller.rb
@@ -0,0 +1,18 @@
+class MessagesController < ApplicationController
+  def index
+    messages = Messages.all
+    messages_array = messages.map do |message|
+      {
+        id: message.id,
+        user_id: message.user.id,
+        name: message.user.name,
+        content: message.content,
+        email: message.user.email,
+        created_at: message.created_at
+      }
+    end
+
+    # messages_arrayをHTTPレスポンスとして返す
+    render json:messages_array, status: 200
+  end
+end
